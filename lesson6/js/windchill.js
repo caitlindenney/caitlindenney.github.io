aaf7012fe2c9ead8ecf59926caaca9c240f7f500
Caitlin Denney <62899200+caitlindenney@users.noreply.github.com>
2020-06-03 14:35:33 -0600

    Add files via upload

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,11 +1,10 @@
-function windchill() {
-    let t = document.getElementById('temp');
-    let s = document.getElementById('speed');
+    let t = document.getElementById('temp').innerHTML;
+
+    let s = document.getElementById('speed').innerHTML;
     if(t<=50&&s>=3.0){
-    let f = 35.74 + 0.6215 * t - (Math.pow(35.75, 0.16)) + (.4275 * t *Math.pow(s, 0.16));
-    System.out.println(f);
+    let f = 35.74 + (0.6215 * t) - (35.75*Math.pow(s, 0.16)) + (.4275 * t *Math.pow(s, 0.16));
+    document.getElementById("chill").innerHTML=f;
     }
     else{
-        document.write("N/A");
+        document.getElementById("chill").innerHTML="N/A";
     }
-}
