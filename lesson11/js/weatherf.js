b3b22339dc07891e84afcc11ed1927c8d3eb584b
Caitlin Denney <62899200+caitlindenney@users.noreply.github.com>
2020-07-04 21:20:34 -0600

    Add files via upload

--- lesson11/js/weatherf.js
+++ lesson11/js/weatherf.js
@@ -4,6 +4,7 @@ const apiURLf = 'https://api.openweathermap.org/data/2.5/forecast?id=5585010&APP
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
+    console.log(jsObject)
     const temp = jsObject.main.temp;
     const speed = jsObject.wind.speed;
     if (temp <= 50 && s >= 3.0) {
@@ -21,22 +22,25 @@ fetch(apiURL)
 fetch(apiURLf)
   .then((response) => response.json())
   .then((jsObject) => {
+    console.log(jsObject)
+    const fiveDaysForecast = jsObject.list.filter(element => element.dt_txt.includes('18:00:00'));
+    
+    const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-    const week = jsObject.list.filter(element => element.dt_txt.includes('18:00:00'));
-
-    console.log(week);
-    let weekdays = ['sun','Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'sat'];
     let day = 0;
-    week.forEach(jsObject => {
-      let d = new Date(jsObject.dt_txt);
-      document.getElementById(`forecast${day+1}`).textContent = jsObject.main.temp;
-      document.getElementById(`day${day+1}`).textContent = weekdays[d.getDay()];
+ 
+    fiveDaysForecast.forEach(forecast => {
+      let d = new Date(forecast.dt_txt);
 
+      document.getElementById(`day-${day}`).textContent = weekDays[d.getDay()];
+      document.getElementById(`forecast-${day}`).textContent = forecast.main.temp_max;
 
-      const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+      let imgScr = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+      document.getElementById(`img-${day}`).setAttribute('src', imgScr);
 
-      document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
-
-
+      let imgAlt = forecast.weather[0].description;
+      document.getElementById(`img-${day}`).setAttribute('alt', imgAlt);
+      day++;
     });
+
   });
